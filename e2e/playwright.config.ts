61f2348d1cc2b92b4189b3fc492a97ad2e708fec
Xabier Napal <xabier.napal@dvzr.io>
2025-07-02 02:27:46 +0200

    ci: improve unit and e2e test workflows

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   fullyParallel: false,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  forbidOnly: !!process.env["CI"],
+  retries: process.env["CI"] ? 2 : 0,
+  workers: process.env["CI"] ? 1 : "50%",
   globalSetup: require.resolve("./global-setup.ts"),
   globalTeardown: require.resolve("./global-teardown.ts"),
   reporter: "html",
